9ef4fcd24323ad72d0fe8f83c256d53c754499c9
Christian Ost <hallo@christianost.de>
2021-04-18 12:33:31 +0200

    fix: remove footer

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import DeckGL from "@deck.gl/react";
 import { LineLayer } from "@deck.gl/layers";
 import { StaticMap } from "react-map-gl";
+
+import styles from "../styles/Home.module.css";
 
 // Viewport settings
 const INITIAL_VIEW_STATE = {
@@ -40,17 +41,6 @@ export default function Home() {
           <StaticMap mapboxApiAccessToken={process.env.mapboxAccessToken} />
         </DeckGL>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
