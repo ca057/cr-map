ec997ad22d1c1991324107243d53d2d6ea0cdb6f
Christian Ost <hallo@christianost.de>
2021-04-18 18:11:54 +0200

    feat: start adding interactivity

--- pages/components/map.tsx
+++ pages/components/map.tsx
@@ -5,10 +5,12 @@ import { GeoJsonLayer } from "@deck.gl/layers";
 import { StaticMap } from "react-map-gl";
 import { JSONLoader } from "@loaders.gl/json";
 
+const extractCountyName = (info) => info?.object?.properties?.kreis_name;
+
 const DEFAULT_INITIAL_VIEW_STATE = {
   longitude: 9.17702,
   latitude: 48.78232,
-  zoom: 13,
+  zoom: 6,
   pitch: 0,
   bearing: 0,
 };
@@ -16,7 +18,7 @@ const DEFAULT_INITIAL_VIEW_STATE = {
 interface Props {
   width: string;
   height: string;
-  initialViewState: {
+  initialViewState?: {
     longitude?: number;
     latitude?: number;
     zoom?: number;
@@ -29,6 +31,7 @@ export default function Map({ width, height, initialViewState }: Props) {
     ...DEFAULT_INITIAL_VIEW_STATE,
     ...initialViewState,
   };
+  const [countyOfInterest, setCountyOfInterest] = useState<string | null>(null);
   const [zoom, setZoom] = useState(mergedInitialViewState.zoom);
   const kreiseLayer = new GeoJsonLayer({
     id: "index-layer",
@@ -39,12 +42,16 @@ export default function Map({ width, height, initialViewState }: Props) {
     filled: true,
     lineWidthScale: 20,
     lineWidthMinPixels: 2,
-    getFillColor: [160, 160, 180, 100],
+    getFillColor: [160, 160, 180, zoom < 10 ? 100 : 0],
     getLineWidth: 1,
-    visible: zoom < 10,
+    visible: true,
+    onHover: (info) => {
+      setCountyOfInterest(extractCountyName(info));
+    },
   });
 
   const indexLayer = new MVTLayer({
+    id: "county-layer",
     data: `${process.env.tileServerBaseUrl}/maps/cargorocket/{z}/{x}/{y}.pbf`,
     minZoom: 10,
     maxZoom: 20,
@@ -61,17 +68,32 @@ export default function Map({ width, height, initialViewState }: Props) {
   const layers = [indexLayer, kreiseLayer];
 
   return (
-    <DeckGL
-      initialViewState={mergedInitialViewState}
-      controller={true}
-      layers={layers}
-      width={width}
-      height={height}
-      onViewStateChange={({ viewState }) => {
-        setZoom(Math.round(viewState.zoom));
-      }}
-    >
-      <StaticMap mapboxApiAccessToken={process.env.mapboxAccessToken} />
-    </DeckGL>
+    <>
+      <DeckGL
+        initialViewState={mergedInitialViewState}
+        controller={true}
+        layers={layers}
+        width={width}
+        height={height}
+        onViewStateChange={({ viewState }) => {
+          setZoom(Math.round(viewState.zoom));
+        }}
+      >
+        <StaticMap mapboxApiAccessToken={process.env.mapboxAccessToken} />
+      </DeckGL>
+      {countyOfInterest && countyOfInterest !== "" && (
+        <div
+          style={{
+            position: "absolute",
+            zIndex: 1000,
+            pointerEvents: "none",
+            left: 0,
+            top: 0,
+          }}
+        >
+          {countyOfInterest}
+        </div>
+      )}
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
 import Head from "next/head";
 
 import Map from "./components/map";
-
 import styles from "../styles/Home.module.css";
-
-// Viewport settings
-const INITIAL_VIEW_STATE = {
-  longitude: 9.17702,
-  latitude: 48.78232,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0,
-};
 
 export default function Home() {
   return (
@@ -22,11 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Map
-          initialViewState={INITIAL_VIEW_STATE}
-          width="100%"
-          height="calc(100vh - 50px)"
-        />
+        <Map width="100%" height="calc(100vh - 50px)" />
       </main>
       <footer className={styles.footer}>
         <p>Cargorocket 2021</p>
